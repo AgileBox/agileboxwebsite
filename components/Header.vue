1b80ff6edcdb6ea6b0344b382ca9e3dd0559bc5b
Ewa Pe <ewa.peronczyk@gmail.com>
2021-04-23 14:25:01 +0200

    Working on navigation

--- components/Header.vue
+++ components/Header.vue
@@ -1,30 +1,41 @@
 <template>
     <header class="header">
-        <nav class="nav">
-            <NuxtLink to="/">
-                <h1>The Blog Title</h1>
-            </NuxtLink>
-            <div>
-                <h1>
-                    <NuxtLink :to="infoRoute">{{ isInfoPage ? "close" : "info" }}</NuxtLink>
-                </h1>
-            </div>
-        </nav>
+        <div class="container mx-auto w-2/4 h-1/5">
+            <nav class="nav">
+                <ul class="flex flex-row justify-evenly items-center">
+                    <li class="nav__items">
+                        <NuxtLink to="/">
+                            Strona główna
+                        </NuxtLink>
+                    </li>
+                    <li class="nav__items">
+                        <NuxtLink to="/">
+                            O nas
+                        </NuxtLink>
+                    </li>
+                    <li class="nav__items">
+                        <NuxtLink to="/">
+                            Oferta
+                        </NuxtLink>
+                    </li>
+                    <li class="nav__items">
+                        <NuxtLink to="/">
+                            Blog
+                        </NuxtLink>
+                    </li>
+                    <li class="nav__items">
+                        <NuxtLink to="/">
+                            Kontakt
+                        </NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </header>
 </template>
 
 <script>
     export default {
-        props: {
-                isInfoPage: {
-                    type: Boolean,
-                    required: false
-            }
-        },
-        computed: {
-            infoRoute() {
-                return this.isInfoPage ? "/" : "/info"
-            }
-        }
+
     }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,25 +5,25 @@
 </template>
 
 <script>
-import BlogList from '~/components/BlogList.vue'
+// import BlogList from '~/components/BlogList.vue'
 
 export default {
     layout: 'layout',
 
-    components: {
-        BlogList
-    },
+    // components: {
+    //     BlogList
+    // },
 
-    async asyncData() {
-        // create context via webpack to map over all blog posts
-        const allPosts = await require.context("~/content/blog-posts/", true, /\.md$/)
-        const posts = allPosts.keys().map((key) => {
-            // give back the value of each post context
-            return allPosts(key)
-        });
-        return {
-            posts
-        }
-    }
+    // async asyncData() {
+    //     // create context via webpack to map over all blog posts
+    //     const allPosts = await require.context("~/content/blog-posts/", true, /\.md$/)
+    //     const posts = allPosts.keys().map((key) => {
+    //         // give back the value of each post context
+    //         return allPosts(key)
+    //     });
+    //     return {
+    //         posts
+    //     }
+    // }
 }
 </script>
