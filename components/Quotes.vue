440a3fd9ac6da393b76976fb3b450cdd43a4e77f
Ewa Pe <ewa.peronczyk@gmail.com>
2021-05-14 13:42:04 +0200

    working on Quotes component

---
+++ components/Quotes.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="wrapper mb-16">
+        <ul class="grid grid-cols-12 gap-8">
+            <li
+                v-for="(item, index) in quotesContent"
+                :key="index"
+                class="col-span-4 p-10 border border-gray-100 rounded-lg shadow-lg text-center text-lg font-medium"
+            >
+                <div class="font-serif text-9xl text-blue-500 -mb-14">
+                    &rdquo;
+                </div>
+                <p class=" text-gray-500 mb-4">
+                    {{ item.text }}
+                </p>
+                <p class="text-blue-500  uppercase mb-4">
+                    {{ item.name }}
+                </p>
+                <p class="text-gray-500 mb-4">
+                    {{ item.function }}
+                </p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        quotesContent: [
+            {
+                text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam laborum harum rerum. Architecto aperiam perspiciatis at alias autem quo ab corporis iusto, sit saepe labore fugiat debitis odio quis voluptates!",
+                name: "Jan Kowalski",
+                function: "CEO"
+            },
+            {
+                text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam laborum harum rerum. Architecto aperiam perspiciatis at alias autem quo ab corporis iusto",
+                name: "Jan Kowalski",
+                function: "CEO"
+            },
+            {
+                text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam laborum harum rerum. Architecto aperiam perspiciatis at alias autem quo ab corporis iusto, sit saepe labore fugiat debitis odio quis voluptates!",
+                name: "Jan Kowalski",
+                function: "CEO"
+            },
+        ]
+    })
+}
+</script>
+
+<style lang="scss">
+// .quote {
+//     font-family: Georgia, 'Times New Roman', Times, serif;
+// }
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
         <Banner />
         <TextAndImage />
         <BlogList :posts="posts" />
-        <div class="bg-gray-50">
+        <div class="bg-gray-50 pb-6">
             <HeaderAndText
                 title="Co robimy"
                 subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit."
@@ -11,12 +11,16 @@
                 <p class="text-center text-gray-500 mb-4">
                     Fugit quas, maiores debitis ut beatae quibusdam, dignissimos aliquid aspernatur temporibus tempore non suscipit necessitatibus alias quidem accusantium voluptatum laborum doloribus quasi? Temporibus, repellat, natus fuga labore expedita ducimus perferendis consequatur facere nostrum voluptatum commodi repellendus itaque, quos modi voluptatibus tempora deleniti iure mollitia.
                 </p>
+                <NuxtLink to="/" class="[ inline-flex px-10 py-3 bg-blue-500 text-white font-medium border border-transparent border-solid rounded-sm hover:bg-black transition-all ] btn__main">
+                            Więcej
+                </NuxtLink>
             </HeaderAndText>
             <HeaderAndText
                 title="Rekomendacje"
                 subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quas, maiores debitis ut beatae quibusdam."
             >
             </HeaderAndText>
+            <Quotes />
         </div>
     </div>
 </template>
@@ -26,6 +30,7 @@ import Banner from '~/components/Banner.vue'
 import TextAndImage from '~/components/TextAndImage.vue'
 import BlogList from '~/components/BlogList.vue'
 import HeaderAndText from '~/components/HeaderAndText.vue'
+import Quotes from '~/components/Quotes.vue'
 
 export default {
     layout: 'layout',
@@ -34,7 +39,8 @@ export default {
         Banner,
         TextAndImage,
         BlogList,
-        HeaderAndText
+        HeaderAndText,
+        Quotes
     },
 
     async asyncData() {
